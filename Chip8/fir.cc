e8917af2a54f8120655d6c972f2f902acccf01ec
ThomasGeorge <t6george@edu.uwaterloo.ca>
2018-05-14 00:59:58 -0400

    handhsaking logic is implemented

--- Chip8/fir.cc
+++ Chip8/fir.cc
@@ -5,19 +5,40 @@ void fir::fir_main(){
   out.write(0);
   wait(); //wait for one clock cycle
 
+  //initialize handshake
+  inp_rdy.write(0); //not ready to accept tb input
+  out_vld.write(0); //not ready to generate a valid output yet
+
   //functionality
   while(true){
+    sc_int<16> in_val;
+    sc_int<16> sum;
+
+    inp_rdy.write(1);
+
+    do{
+      wait();
+    }while(!inp_vld.read());
+
+    in_val = inp.read();
+    inp_rdy.write(0);
+
     //shift register
     sc_int<16> taps[5];
     for(int i = 4; i >= 1; i--){
       taps[i] = taps[i-1];
     }
-    taps[0] = inp.read();
-    sc_int<16> sum;
+    taps[0] = in_val;
+    
     for(int i = 0; i < 5; i++){
       sum += taps[i];
     }
+
+    out_vld.write(1);
     out.write(sum);
-    wait();
+    do{
+      wait();
+    }while(!out_rdy.read());
+    out_vld.write(0);
   }
 }
--- Chip8/tb.cc
+++ Chip8/tb.cc
@@ -2,6 +2,7 @@
 
 void tb::source(){ //sends test signals to SC_MODULE
     inp.write(0); //send input to fir
+    inp_vld.write(0); //initial input from tb is invalid
     rst.write(1); //reset fir
     wait(); //ensure reset by allowing one clock cycle to pass
     rst.write(0); //fir now available to be set
@@ -12,17 +13,32 @@ void tb::source(){ //sends test signals to SC_MODULE
     for(int i = 0; i< 64; i++){
         if(i>25 && i<32)temp = 256;
         else temp = 0;
-        inp.write(temp); //generates a square wave input
-        wait();
+        inp_vld.write(1); //input for fir has been correctly generated
+        inp.write(temp); //make input available on tb pin
+        do{
+            wait(); //keep waiting through clk periods until
+        }while(!inp_rdy.read()); //fir says its ready to accept input
+        inp_vld.write(0); //invalidate input from tb
+        
     }
 }
 
 void tb::sink(){ //recieves resultant outputs from tested SC_MODULE
     sc_int<16> in_data;
 
+    //initialize handshake
+    out_rdy.write(0);
+
+
     for(int i = 0; i<64; i++){
+        out_rdy.write(1);
+        do{
+            wait();
+        }while(!out_vld.read());
+
         in_data = out.read(); //read output data from SC_MODULE
-        wait();
+        out_rdy.write(0);
+
     }
     sc_stop(); //end simulation of SYSTEM
 }
