edad44f83a11372262a6c3e5fdac1a13e23a2089
ThomasGeorge <t6george@edu.uwaterloo.ca>
2018-06-11 22:04:55 -0400

    made screen drawing OO

--- Chip8/peripherals.cc
+++ Chip8/peripherals.cc
@@ -12,13 +12,14 @@ Peripherals::Peripherals(): FPS(60.0){
         this->gfx[i][j] = 0;
       }
   }
+	this->gfx[2][5] = 1;
 
 	this->timer = al_create_timer(1.0 / FPS);
 	if (!timer) {
 		cerr << "Failed to create timer." << endl;
 	}
 
-	this->display = al_create_display(640, 320);
+	this->display = al_create_display(1280, 640);
 	if (!display) {
 		cerr << "Failed to create display." << endl;
 	}
@@ -27,60 +28,55 @@ Peripherals::Peripherals(): FPS(60.0){
 	if (!event_queue) {
 		cerr << "Failed to create event queue." << endl;
 	}
-
-	this->running = true;
-	this->redraw = true;
-}
-
-Peripherals::~Peripherals(){
-  delete [] gfx;
-}
-
-
-void Peripherals::updateDisplay(){
 	al_register_event_source(event_queue, al_get_display_event_source(display));
 	al_register_event_source(event_queue, al_get_timer_event_source(timer));
 
 	al_clear_to_color(al_map_rgb(0, 0, 0));
 	al_flip_display();
 
+	al_init_timeout(&timeout, 0.06);
 	al_start_timer(timer);
 
-	while (running) {
-		ALLEGRO_EVENT event;
-		ALLEGRO_TIMEOUT timeout;
+	this->running = true;
+}
 
-		al_init_timeout(&timeout, 0.06);
+Peripherals::~Peripherals(){
+	al_destroy_display(this->display);
+	al_destroy_event_queue(this->event_queue);
+  delete [] gfx;
+}
 
-		bool get_event = al_wait_for_event_until(event_queue, &event, &timeout);
 
-		if (get_event) {
-			switch (event.type) {
-				case ALLEGRO_EVENT_TIMER:
-					redraw = true;
-					break;
-				case ALLEGRO_EVENT_DISPLAY_CLOSE:
-					running = false;
-					break;
-				default:
-					cerr << "Unsupported event received." << endl;
-					break;
+void Peripherals::updateDisplay(){
+
+	get_event = al_wait_for_event_until(event_queue, &event, &timeout);
+	
+	if (get_event) {
+		switch (event.type) {
+			case ALLEGRO_EVENT_TIMER:
+				break;
+			case ALLEGRO_EVENT_DISPLAY_CLOSE:
+				this->running = false;
+				cout << "test" << endl;
+				break;
+			default:
+				cerr << "Unsupported event received." << endl;
+				break;
+		}
+	}
+
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+
+	for(int i = 0; i < 64; i++){
+		for(int j = 0; j < 32; j++){
+			if(this->gfx[j][i] == 1){
+				al_draw_filled_rectangle(i*20,j*20,i*20+20,j*20+20,al_map_rgb(255,255,255));
 			}
 		}
+	}
 
-		al_clear_to_color(al_map_rgb(0, 0, 0));
-		for(int i = 0; i < 64; i++){
-			for(int j = 0; j < 32; j++){
-				if(this->gfx[j][i] == 1){
-					al_draw_filled_rectangle(i*10,j*10,i*10+10,j*10+10,al_map_rgb(255,255,255));
-				}
-			}
-  }
-		al_flip_display();
-	} 
+	al_flip_display();
 
-	al_destroy_display(display);
-	al_destroy_event_queue(event_queue);
 }
 
 int main(int argc, char*argv[]){
@@ -88,8 +84,13 @@ int main(int argc, char*argv[]){
 		cerr << "Failed to initialize allegro." << endl;
 		return 1;
 	}
+
   Peripherals* peripheral = new Peripherals();
-  peripheral->updateDisplay();
+
+	while(peripheral->running){
+		peripheral->updateDisplay();
+	}
+
   return 0;
 }
 
--- Chip8/peripherals.h
+++ Chip8/peripherals.h
@@ -11,13 +11,16 @@ class Peripherals{
   private:
     short**gfx;
     const float FPS;
-    bool running;
-    bool redraw;
-    ALLEGRO_DISPLAY *display;
-    ALLEGRO_EVENT_QUEUE *event_queue;
     ALLEGRO_TIMER *timer;
+    ALLEGRO_TIMEOUT timeout;
+		ALLEGRO_EVENT event;
+    ALLEGRO_EVENT_QUEUE *event_queue;
+    ALLEGRO_DISPLAY *display;
+    bool get_event;
 
   public:
+    bool running;
+
     Peripherals();
     ~Peripherals();
     void updateDisplay();
